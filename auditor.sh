78362f70c81ac917bd1ceb760bea39581cd3cfa5
Thrishar07 <thrishar915@gmail.com>
2026-09-22 16:26:55 +0000

    initial

---
+++ auditor.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo "Linux Security Compliance Auditor"
+echo "starting the security audit......"
+echo "System:$(hostname)"
+echo "User:$(whoami)"
+echo "Date:$(date)"
+
+echo "initialized..."
+
